ffcb24cbe11adc50dc283cf453c9976fbe76a36c
Berserker <luanbraga.lb@gmail.com>
2021-03-15 17:28:32 -0300

    Update list.php

--- application/views/clientes/list.php
+++ application/views/clientes/list.php
@@ -24,34 +24,52 @@ $this->load->helper('url');
         <a href="clientes/cadastro" class="btn btn-success pull-right">Cadastrar Cliente</a>
         <div class="form-inline">
             <div class="input-group">
-                <input type="text" class="form-control" placeholder="Sua busca aqui">
+                <input type="text" class="form-control" placeholder="Sua busca aqui" id="texto">
                 <span class="input-group-btn">
-                    <button class="btn btn-default" type="button">Buscar</button>
+                    <button class="btn btn-default" type="button" id="buscar">Buscar</button>
                 </span>
             </div>
         </div>
         <br>
-        <table class="table table-bordered table-striped">
-            <thead>
-                <th>Id</th>
-                <th>Nome</th>
-                <th>Email</th>
-                <th>Cadastro em</th>
-                <th>Adm</th>
-            </thead>
-            <?php foreach ($clientes as $cliente) : ?>
-                <tr>
-                    <td><?=$cliente['id']?></td>
-                    <td><?=$cliente['nome']?></td>
-                    <td><?=$cliente['email']?></td>
-                    <td><?=date('d/m/Y', strtotime($cliente['cadastrado_em']))?></td>
-                    <td>
-                        <a href="<?= site_url(['clientes', 'update', $cliente['id']])?>" class="btn btn-primary">Alterar</a>
-                        <a href="<?= site_url(['clientes', 'delete', $cliente['id']])?>" class="btn btn-danger">Excluir</a>
-                    </td>
-                </tr>
-            <?php endforeach; ?>
-        </table>
+        <div id="table_ajax">
+            <table class="table table-bordered table-striped">
+                <thead>
+                    <th>Id</th>
+                    <th>Nome</th>
+                    <th>Email</th>
+                    <th>Cadastro em</th>
+                    <th>Adm</th>
+                </thead>
+                <?php foreach ($clientes as $cliente) : ?>
+                    <tr>
+                        <td><?=$cliente['id']?></td>
+                        <td><?=$cliente['nome']?></td>
+                        <td><?=$cliente['email']?></td>
+                        <td><?=date('d/m/Y', strtotime($cliente['cadastrado_em']))?></td>
+                        <td>
+                            <a href="<?= site_url(['clientes', 'update', $cliente['id']])?>" class="btn btn-primary">Alterar</a>
+                            <a href="<?= site_url(['clientes', 'delete', $cliente['id']])?>" class="btn btn-danger">Excluir</a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
+        </div>
     </div>
 </div>
 <?php $this->load->view('template/footer'); ?>
+<script>
+    $('#buscar').click(function (){
+        if($('#texto').val() == ''){
+            return
+        }
+
+        $.ajax({
+            url: 'http://[::1]/codeigniterapp/index.php/clientes/search/' + $('#texto').val(),
+            type: 'GET',
+            success: function (data) {
+                $('#table_ajax').empty();
+                $('#table_ajax').html(data);
+            }
+        });
+    });
+</script>
