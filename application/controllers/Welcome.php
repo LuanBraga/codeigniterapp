6b9f95173097f987aec1a242089de407f810dfe1
Berserker <luanbraga.lb@gmail.com>
2021-03-15 17:27:45 -0300

    Update Welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends MY_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -20,7 +20,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->database();
+		$this->require_min_level(1);
 		$this->load->view('home');
 	}
 
